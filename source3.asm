36a3aaff6723efd91060d88fafb14712c0aa6a10
bkhactri <bkhactri@gmail.com>
2020-05-14 10:29:59 +0700

    them source3

---
+++ source3.asm
@@ -0,0 +1,32 @@
+.data 
+	tb1: .asciiz "project2"
+	tb2: .asciiz "tuan da o day"
+
+.text
+	li $v0,4
+	la $a0,tb1
+	syscall 
+	
+	li $v0, 4
+	la $a0, tb2
+	syscall
+
+	li $v0,4
+	la $a0,tb1
+	syscall 
+	
+	li $v0, 4
+	la $a0, tb2
+	syscall
+
+
+	li $v0,4
+	la $a0,tb1
+	syscall 
+	
+	li $v0, 4
+	la $a0, tb2
+	syscall
+
+
+
