c88dc1700307a9fe96a201285896a0e0a72cb8cc
bkhactri <bkhactri@gmail.com>
2020-05-14 11:10:03 +0700

    pull ve cbi lam

--- source.asm
+++ source.asm
@@ -1,18 +1,8 @@
 .data 
 	tb1: .asciiz "project2"
-	tb2: .asciiz "tuan da o day"
-	tb3: .asciiz "\n"
-	tb5: .asciiz "tri da o day"
 
 .text
 	li $v0,4
 	la $a0,tb1 
 	syscall 
 	
-	li $v0, 4
-	la $a0, tb2
-	syscall
-	
-	li $v0, 4
-	la $a0, tb3
-	syscall
